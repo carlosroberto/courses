24c84acda0e32e0afb908558382461172a15d904
Carlos Junior <carlosroberto366@hotmail.com>
2022-03-04 14:12:13 -0300

    removing this.state structure

--- react/ciclo-vida-componentes/ciclo-vida-componentes/src/App.js
+++ react/ciclo-vida-componentes/ciclo-vida-componentes/src/App.js
@@ -4,17 +4,23 @@ import FormularioCadastro from "./components/FormularioCadastro";
 import ListaDeCategorias from "./components/ListaDeCategorias";
 import "./assets/App.css";
 import './assets/index.css';
+import Categorias from "./Dados/Categorias";
+import ArrayDeNotas from "./Dados/Notas";
 class App extends Component {
 
   constructor() {
     super();
 
-    this.state = {
-      notas: [],
-      categorias: [],
-    }
+    this.categorias = new Categorias()
+    this.notas = new ArrayDeNotas()
+  
+ // retirando this.state para que os dados sejam passados pelas classes acima, porem nao havera mais renderização em cascata
+ //    this.state = {
+ //     notas: [],
+ //      categorias: [],
+ //   }
   }
-
+/*
   criarNota(titulo, texto, categoria) {
     const novaNota = { titulo, texto, categoria };
     const novoArrayNotas = [...this.state.notas, novaNota]
@@ -36,21 +42,21 @@ class App extends Component {
     const novoEstado = {...this.state, categorias:novoArrayCategorias} //estado atual + nova categoria
     this.setState(novoEstado)
   }
-
+*/
   render() {
     return (
       <section className="conteudo">
         <FormularioCadastro 
-        categorias={this.state.categorias}
-        criarNota={this.criarNota.bind(this)} />
+        categorias={this.categorias.categorias}
+        criarNota={this.notas.criarNota} />
         <main className="conteudo-principal">
           <ListaDeCategorias 
-            adicionarCategoria={this.adicionarCategoria.bind(this)}
-            categorias={this.state.categorias}
+            adicionarCategoria={this.categorias.adicionarCategoria}
+            categorias={this.categorias.categorias}
           />
           <ListaDeNotas
-            apagarNota={this.deletarNota.bind(this)}
-            notas={this.state.notas} 
+            apagarNota={this.notas.apagarNota}
+            notas={this.notas.notas} 
           />
         </main>
       </section>
